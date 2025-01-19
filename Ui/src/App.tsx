c108ec88c1e00644b26a3117a69390ea29b98138
Hendra Dwi Saputra <hendra@usa.com>
2025-01-19 15:29:40 +0700

    Initialize React application with Vite and Tailwind CSS setup

---
+++ Ui/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <main className="flex justify-center items-center h-screen">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
