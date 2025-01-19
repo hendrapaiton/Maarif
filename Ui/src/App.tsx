14574b76a05c501d8ef3bbabf1de7618fbf2dcb9
Hendra Dwi Saputra <hendra@usa.com>
2025-01-19 17:33:34 +0700

    Add Gugi font and Bootstrap icons; update App component and styles

--- Ui/src/App.tsx
+++ Ui/src/App.tsx
@@ -2,7 +2,10 @@ function App() {
   return (
     <>
       <main className="flex justify-center items-center h-screen">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1 className="text-3xl font-bold underline">
+          <i className="bi bi-heart-fill text-red-500 me-3"></i>
+          <span>Halu Dunia</span>
+        </h1>
       </main>
     </>
   );
